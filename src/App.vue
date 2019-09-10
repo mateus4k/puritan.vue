0aa172405145867f9b35d6ea325af67348a9c0ad
mateus4k <mateus4k@protonmail.ch>
2019-09-09 22:11:08 -0300

    :rocket: Home e style

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,62 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <theHeader />
+    <router-view class="container"></router-view>
   </div>
 </template>
 
+<script>
+import TheHeader from "@/components/TheHeader.vue";
+
+export default {
+  components: {
+    TheHeader
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  margin: 0px;
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  color: #234;
 }
-#nav {
-  padding: 30px;
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
 }
-
-#nav a {
+a {
   font-weight: bold;
-  color: #2c3e50;
+  text-decoration: none;
+  color: #234;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+a.router-link-exact-active {
+  color: #4b8;
+}
+img {
+  max-width: 100%;
+}
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+.v-enter {
+  opacity: 0;
+  transform: translate3d(-20px, 0, 0);
+}
+.v-enter-active {
+  transition: all 0.3s;
+}
+.conteudo {
+  display: grid;
+  grid-template-columns: 1fr minmax(200px, 400px);
+  grid-gap: 30px;
+}
+@media screen and (max-width: 576px) {
+  .conteudo {
+    display: block;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="home">
+    <img alt="Vue logo" src="@/assets/logo.png" />
+    <h1>Styled Components no Vue.js</h1>
+    <div>
+      <CButton>Clique Aqui</CButton>
+      <CButtonProps primary>Clique Aqui</CButtonProps>
+      <CButtonPropsCond>Clique Aqui Condicional</CButtonPropsCond>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import CButton from "@/components/Button";
+import CButtonProps from "@/components/ButtonProps";
+import CButtonPropsCond from "@/components/ButtonPropsCond";
 export default {
   name: "home",
   components: {
-    HelloWorld
+    CButton,
+    CButtonProps,
+    CButtonPropsCond
   }
 };
 </script>
+
+<style>
+#home {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
